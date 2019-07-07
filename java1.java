3137463d9161fdd7ba0dcfa5d34edc79d2df2270
Navjot98727 <52626971+Navjot98727@users.noreply.github.com>
2019-07-07 09:33:23 -0400

    Create java1.java

---
+++ java1.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        // write a program that ads A to * and store the result in C
+        // where A=3, B=4, C=5
+    }//end of main
+}//end of class

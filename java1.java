f724dbc4863c8d30444d2301f7b55933408b588e
Navjot98727 <52626971+Navjot98727@users.noreply.github.com>
2019-07-07 09:54:05 -0400

    Update java1.java

--- java1.java
+++ java1.java
@@ -3,6 +3,6 @@ public class Main {
         // write a program that ads A to * and store the result in C
         // where A=3, B=4, C=5
         int A=3, B=4, C=A+B;
-        
+        System.out.println("The result is "+C);
     }//end of main
 }//end of class

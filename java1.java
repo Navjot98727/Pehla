b67a0c84708a1cf34edc5d189807f115ac155e5e
Navjot98727 <52626971+Navjot98727@users.noreply.github.com>
2019-07-07 09:52:07 -0400

    Update java1.java

--- java1.java
+++ java1.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         // write a program that ads A to * and store the result in C
         // where A=3, B=4, C=5
+        int A=3, B=4, C=A+B;
+        
     }//end of main
 }//end of class
